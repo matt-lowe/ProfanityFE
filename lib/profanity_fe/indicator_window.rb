932c5d5419dcd081cd1108361ff129031ea9d639
Matt Lowe <matt@io4.us>
2019-02-23 10:53:01 -0600

    retab

--- lib/profanity_fe/indicator_window.rb
+++ lib/profanity_fe/indicator_window.rb
@@ -1,48 +1,48 @@
 class IndicatorWindow < Curses::Window
-	@@list = Array.new
+   @@list = Array.new
 
-	def IndicatorWindow.list
-		@@list
-	end
+   def IndicatorWindow.list
+      @@list
+   end
 
-	attr_accessor :fg, :bg, :layout
-	attr_reader :label, :value
+   attr_accessor :fg, :bg, :layout
+   attr_reader :label, :value
 
-	def label=(str)
-		@label = str
-		redraw
-	end
+   def label=(str)
+      @label = str
+      redraw
+   end
 
-	def initialize(*args)
-		@fg = [ '444444', 'ffff00' ]
-		@bg = [ nil, nil ]
-		@label = '*'
-		@value = nil
-		@@list.push(self)
-		super(*args)
-	end
-	def update(new_value)
-		if new_value == @value
-			false
-		else
-			@value = new_value
-			redraw
-		end
-	end
+   def initialize(*args)
+      @fg = [ '444444', 'ffff00' ]
+      @bg = [ nil, nil ]
+      @label = '*'
+      @value = nil
+      @@list.push(self)
+      super(*args)
+   end
+   def update(new_value)
+      if new_value == @value
+         false
+      else
+         @value = new_value
+         redraw
+      end
+   end
 
-	def redraw
-		setpos(0,0)
-		if @value
-			if @value.is_a?(Integer)
-				attron(Curses::color_pair(get_color_pair_id(@fg[@value], @bg[@value]))|Curses::A_NORMAL) { addstr @label }
-			else
-				attron(Curses::color_pair(get_color_pair_id(@fg[1], @bg[1]))|Curses::A_NORMAL) { addstr @label }
-			end
-		else
-			attron(Curses::color_pair(get_color_pair_id(@fg[0], @bg[0]))|Curses::A_NORMAL) { addstr @label }
-		end
-		noutrefresh
-		true
-	end
+   def redraw
+      setpos(0,0)
+      if @value
+         if @value.is_a?(Integer)
+            attron(Curses::color_pair(get_color_pair_id(@fg[@value], @bg[@value]))|Curses::A_NORMAL) { addstr @label }
+         else
+            attron(Curses::color_pair(get_color_pair_id(@fg[1], @bg[1]))|Curses::A_NORMAL) { addstr @label }
+         end
+      else
+         attron(Curses::color_pair(get_color_pair_id(@fg[0], @bg[0]))|Curses::A_NORMAL) { addstr @label }
+      end
+      noutrefresh
+      true
+   end
 end
 
